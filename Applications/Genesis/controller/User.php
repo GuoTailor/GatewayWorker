8c37e71c9d260ad9ff839f698d57c744152ea5d8
TMD <2350871838@qq.com>
2018-12-13 09:52:34 +0800

    first commit

--- Applications/Genesis/controller/User.php
+++ Applications/Genesis/controller/User.php
@@ -52,7 +52,7 @@ class User
             return TTPublic::getResponse(TTCode::TT_INVALID_DATA);
         }
         $info_req = TTUser::login($mobile, $password, $access_token, $device_token);
-        PhpLog::Log("info: ".$info_req);
+        PhpLog::Log("登录info: ".$info_req);
         return $info_req;
     }
 
